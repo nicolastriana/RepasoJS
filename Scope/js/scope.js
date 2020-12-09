61f167c81ea209bf49c30fb5edcdd0825c31086f
Nicolas Triana <nicolasmutis@hotmail.com>
2020-12-08 19:40:32 -0500

    Scope JS

---
+++ Scope/js/scope.js
@@ -0,0 +1,22 @@
+// Scope
+
+// var musica = 'Rock';
+
+// if(musica){
+//     var musica = 'Grunge';
+//     console.log('Dentro del IF: ', musica);
+// }
+
+// console.log('Fuera del IF: ', musica);
+
+
+// Scope con let
+
+let musica = 'Rock';
+
+if(musica){
+    let musica = 'Grunge';
+    console.log('Dentro del IF: ', musica);
+}
+
+console.log('Fuera del IF: ', musica);
