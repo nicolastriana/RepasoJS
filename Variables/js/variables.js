7916877338c170ae979ef228bf733fe891894ba1
Nicolas Triana <nicolasmutis@hotmail.com>
2020-12-08 19:16:41 -0500

    Variables JS

---
+++ Variables/js/variables.js
@@ -0,0 +1,33 @@
+// Crear variables
+
+// Variables con var
+var aprendiendo99 = true;
+
+aprendiendo99 = false;
+aprendiendo99 = 20;
+aprendiendo99 = 'Nicolas';
+
+// Se pueden crear variables sin asignarle valor 
+
+var aprendiendo;
+
+aprendiendo = 'JavaScript';
+
+console.log(aprendiendo99);
+console.log(aprendiendo);
+
+// Variables con const
+
+const aprendiendo22 = 'JavaScript';
+
+// Al ser una constante sus valores no cambian 
+
+console.log(aprendiendo22);
+
+// Variables con let
+
+let aprendiendo33 = 'JavaScript';
+
+aprendiendo33 = true;
+
+console.log(aprendiendo33);
